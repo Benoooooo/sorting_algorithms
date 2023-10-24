da4662f91b86b331d491bd936fe445b99edfa889
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-24 03:00:48 +0100

    the code

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,24 @@
+/*
+ * file: 1-insertion_sort_list.c
+ * Auth: kelechi nnadi && nonku yandah
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of
+ *                       in ascending order using the insertion algorithm
+ *                       we are not allowed to modify the integer n of a
+ *                       node, only have to swap the nodes themselves.
+ *                       also expected to print the list after each time
+ *                       you swap two elements
+ * @list: the pointer to the  list to be swaped
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int i, j;
+
+	while 
+}
